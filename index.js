cec6b26b4d383bfa99252ca60fcc5e7b26fc2870
Roger Andersen <hexjelly@hexjelly.com>
2017-09-16 12:22:55 +0200

    add: routing test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const app = express();
 
-let app = express();
+// Routes
+app.use(require("./routes"));
 
 app.listen(3000, function() {
 	console.log("Listening on port 3000...");
