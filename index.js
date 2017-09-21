f4dac0efb4baa85be26c6781138614a5c86a95eb
Roger Andersen <hexjelly@hexjelly.com>
2017-09-21 14:14:07 +0200

    add(package): add dotenv-extended, default env and schema files

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv-extended").load({ errorOnMissing: true });
 const express = require("express");
 const app = express();
 
 // Routes
 app.use(require("./routes"));
 
-app.listen(3000, function() {
-	console.log("Listening on port 3000...");
+app.listen(process.env.PORT, function() {
+	console.log(`Listening on port ${process.env.PORT}...`);
 });
