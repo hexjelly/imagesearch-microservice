6e96863a1961422634de4f3fde8781665f71f7b5
Roger Andersen <hexjelly@hexjelly.com>
2017-09-21 12:25:20 +0200

    update: move controller/model logic into routes directly

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+// const GoogleImages = require("google-images");
+// const client = new GoogleImages("CSE ID", "API KEY");
+
+async function imageSearch (keyword, history, page) {
+	if (!keyword) throw new Error("Missing search keyword");
+	if (!history || !Array.isArray(history)) throw new Error("Missing history array");
+	history.push(keyword);
+	history.splice(0, history.length - 10);
+	// const searchResult = await client.search(keyword, { page });
+	// return searchResult;
+	return keyword;
+}
+
 router.get("/", (req, res) => {
 	res.send("api index");
 });
 
-router.get("/imagesearch", (req, res) => {
-	res.send("api imagesearch");
+const searchHistory = [];
+
+router.get("/imagesearch/:keyword", async (req, res) => {
+	try { res.send(await imageSearch(req.params.keyword, searchHistory, req.query.page)); }
+	catch(e) { res.status(400).send({ error: e.message }); }
 });
 
 router.get("/latest", (req, res) => {
