3d0b22808190c77e857ce82c71cb989fb1572d4f
Roger Andersen <hexjelly@hexjelly.com>
2017-09-25 17:09:28 +0200

    update(imageSearch): set up Google API credentials, make it return actual results

--- src/models/api/imageSearch.js
+++ src/models/api/imageSearch.js
@@ -1,5 +1,5 @@
-// const GoogleImages = require("google-images");
-// const client = new GoogleImages("CSE ID", "API KEY");
+const GoogleImages = require("google-images");
+const client = new GoogleImages(process.env.G_CSE_ID, process.env.G_API_KEY);
 
 async function imageSearch(searchKeyword, historyArray, pageOffset) {
 	if (!searchKeyword) throw new Error("Missing search keyword");
@@ -8,9 +8,17 @@ async function imageSearch(searchKeyword, historyArray, pageOffset) {
 	const logEntry = { searchKeyword, date: new Date() };
 	historyArray.unshift(logEntry);
 	console.log(`${logEntry.date}: New search for "${logEntry.searchKeyword}"`);
-	// const searchResult = await client.search(keyword, { page: pageOffset });
-	// return searchResult;
-	return searchKeyword;
+	const searchResult = await client.search(searchKeyword, { page: pageOffset });
+	return searchResult.map(reformatResults);
+}
+
+function reformatResults(elem) {
+	return {
+		url: elem.url,
+		thumbnail: elem.thumbnail.url,
+		description: elem.description,
+		context: elem.parentPage
+	};
 }
 
 module.exports = imageSearch;
