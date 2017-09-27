87f96f5fbefb88837b7aa45d145512fe71ef4630
Roger Andersen <hexjelly@hexjelly.com>
2017-09-27 03:44:35 +0200

    add(test): basic integration test placeholders

---
+++ test/integration/routes.js
@@ -0,0 +1,14 @@
+import supertest from "supertest";
+import test from "ava";
+
+test("api/imagesearch/", t => {
+	t.fail();
+});
+
+test("api/latest/", t => {
+	t.fail();
+});
+
+test("/", t => {
+	t.fail();
+});
