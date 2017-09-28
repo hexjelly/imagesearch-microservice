c9f827135cd4bacce902296bd2a00fb968565bf0
Roger Andersen <hexjelly@hexjelly.com>
2017-09-28 21:23:37 +0200

    add(test): add simple unit tests for imageSearch

--- test/unit/imageSearch.js
+++ test/unit/imageSearch.js
@@ -1,5 +1,17 @@
+import dotenv from "dotenv-extended";
+dotenv.load({ errorOnMissing: true });
 import test from "ava";
+import imageSearch from "../../src/models/api/imageSearch";
 
-test("imageSearch", t => {
-	t.fail();
+test("imageSearch: not passing in search word throws error", async t => {
+	const error = await t.throws(imageSearch());
+	t.is(error.message, "Missing search keyword");
+});
+
+test("imageSearch: not passing in array throws error", async t => {
+	const error = await t.throws(imageSearch("test"));
+	t.is(error.message, "Missing history array");
+
+	const invalid = await t.throws(imageSearch("test", "notanarray"));
+	t.is(invalid.message, "Missing history array");
 });
