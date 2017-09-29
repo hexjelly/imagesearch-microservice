7aaa3a7acb96660fad4006c48267a0278ed00713
Roger Andersen <hexjelly@hexjelly.com>
2017-09-29 04:28:55 +0200

    update: change to arrow function

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv-extended").load({ errorOnMissing: true });
 const server = require("./app");
 
-server.listen(process.env.PORT, function() {
+server.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}...`);
 });
